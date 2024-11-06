5eb830c741a3cabe5bf6269c5b6ec091243e0915
Даня <dattesuki@mail.ru>
2024-11-06 23:59:19 +0300

    sample stack

--- include/stack_queue.h
+++ include/stack_queue.h
@@ -1,5 +1,6 @@
-﻿//реализация ветктора
+//реализация ветктора
 //выбрана реализация на динамическом массиве
+#include <algorithm>
 
 template <typename T>
 class Vector {
@@ -10,30 +11,35 @@ private:
 
 public:
 	Vector(size_t _capacity = 0) :sz(0), capacity(_capacity) {
-		if (_capacity < 0) throw bad_alloc("bad_alloc");
+		if (_capacity < 0) throw std::bad_alloc();
 		if (capacity == 0) capacity = 10;
 		pMem = new T[capacity];
-		if (pMem == nullptr) throw bad_alloc("bad_alloc");
+		if (pMem == nullptr) throw std::bad_alloc();
 	}
 
-	Vector(Vector<T> v) :sz(v.sz), capacity(v.capacity) {
+    Vector(Vector const &v) :sz(v.sz), capacity(v.capacity) {
 		pMem = new T[capacity];
-		if (pMem == nullptr) throw bad_alloc("bad_alloc");
+		if (pMem == nullptr) throw std::bad_alloc();
 		copy(v.pMem, v.pMem+(sz * sizeof(T)), v.pMem);
 	}
 
 	Vector(T* arr, size_t size_array):sz(size_array), capacity(size_array) {
-		if (capacity <= 0) throw bad_alloc("bad_alloc");
+		if (capacity <= 0) throw std::bad_alloc();
 		pMem = new T[capacity];
-		if (pMem == nullptr) throw bad_alloc("bad_alloc");
-		copy(arr, arr + (sz * sizeof(T)), pMem);
+		if (pMem == nullptr) throw std::bad_alloc();
+		std::copy(arr, arr + (sz * sizeof(T)), pMem);
 	}
 
+    ~Vector(){
+        delete[] pMem;
+        pMem=nullptr;
+    }
+    
 	//перевыделение памяти
 	void Overexpression() {
 		T* temp_pMem = new T[(capacity * 2)];
-		if (temp_pMem == nullptr) throw bad_alloc("bad_alloc");
-		copy(pMem, pMem + (sz * sizeof(T)), temp_pMem); //копирование из pMem в temp_pMem
+		if (temp_pMem == nullptr) throw std::bad_alloc();
+		std::copy(pMem, pMem + (sz * sizeof(T)), temp_pMem); //копирование из pMem в temp_pMem
 		std::swap(pMem, temp_pMem);
 		capacity *= 2;
 		delete[] temp_pMem;
@@ -49,35 +55,35 @@ public:
 		else return false;
 	}
 
-	void push_back(T el) {
-		if (IsFull) Overexpression;
+	virtual void push_back(T el) {
+		if (IsFull()) Overexpression();
 		pMem[sz++] = el;
 	}
 
-	void push_front(T el) {
-		if (IsFull) Overexpression;
+    virtual void push_front(T el) {
+		if (IsFull()) Overexpression();
 		for (size_t i = 0; i < sz; ++i) pMem[i + 1] = pMem[i];
 		pMem[0] = el;
 		++sz;
 	}
 
-	T back() { 
-		if (IsEmpty) throw logic_error("error");
-		return pMem[sz - 1]; 
+    virtual T back() {
+		if (IsEmpty()) throw std::logic_error("error");
+		return pMem[sz - 1];
 	}
 
-	T front() {
-		if (IsEmpty) throw logic_error("error");
+    virtual T front() {
+		if (IsEmpty()) throw std::logic_error("error");
 		return pMem[0];
 	}
 
-	void pop_back() {
-		if (IsEmpty) logic_error("error");
+    virtual void pop_back() {
+		if (IsEmpty()) std::logic_error("error");
 		--sz;
 	}
 
-	void pop_front() {
-		if (IsEmpty) logic_error("error");
+    virtual void pop_front() {
+		if (IsEmpty()) std::logic_error("error");
 		sz--;
 		for (size_t i = 0; i < (sz); ++i) pMem[i] = pMem[i + 1];
 	}
@@ -87,7 +93,7 @@ public:
 
 
 template <typename T>
-class Stack: public Vector {
+class Stack: public Vector<T> {
 private:
 	//переопределение незадействованных методов
 	void push_front(T el) override{};
--- samples/sample_stack.cpp
+++ samples/sample_stack.cpp
@@ -1,12 +1,50 @@
-﻿// Тестирование стека
+// Тестирование стека
 
 #include <iostream>
-//#include "tmatrix.h"
+#include "stack_queue.h"
+
+using namespace std;
+
 //---------------------------------------------------------------------------
+bool IsOpenBracket(char t){
+    if (t=='(' or t=='[' or t=='{') return 1;
+    return 0;
+}
 
-int main()
-{
+bool IsCloseBracket(char t){
+    if (t==')' or t==']' or t=='}') return 1;
+    return 0;
+}
 
-  return 0;
+char GetOpenBracket(char t){
+    if (t==')') return '(';
+    if (t==']') return '[';
+    if (t=='}') return '{';
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    string Expression;
+    cin>>Expression;
+    Stack<char> stack;
+    int flag=0;
+    
+    for(int i=0;i<Expression.length();i++){
+        if (IsOpenBracket(Expression[i])){
+            stack.push_back(Expression[i]);
+        }
+        if(IsCloseBracket(Expression[i])){
+            if (stack.IsEmpty()) flag++;
+            else{
+                if (stack.back()!=GetOpenBracket(Expression[i]))flag++;
+                else{
+                    if (stack.IsEmpty() == 0) (stack.pop_back());
+                    else flag++;
+                }}}}
+          
+    if(stack.IsEmpty()!=1) flag++;
+    if (flag==0) cout<<"\nВсе хорошо!";
+    else cout<<"\nГде-то ошибка(";
+    return 0;
 }
 //---------------------------------------------------------------------------
